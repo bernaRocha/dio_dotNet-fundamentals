5d61b919e0b3c55bd06f7b16e3c98b2dbfe39ad5
Bernardo <mrochabernardo@gmail.com>
2022-07-05 10:55:32 -0300

    começo do trabalho

---
+++ dio.series/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace dio.series
+{
+    public abstract class EntidadeBase
+    {
+        public Id { get; protected set; } // todas as clases que herdarem terão o identificador do objeto que é o id
+    }
+}
---
+++ dio.series/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dio.series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serie meuObjeto = new Serie(); //deixa criar o objeto
+            
+        }
+    }
+}
